5614e6611f383486a8d55e9c9d2e6bb9146114bc
aaronBurns59 <aaronburns59@gmail.com>
2019-09-09 22:33:32 +0100

    Created a function that determines the size of input based on bytes e.g kilobytes, megabytes, gigabytes etc

--- measureFileSize.py
+++ measureFileSize.py
@@ -1,2 +1,25 @@
+# Constant that holds the prefixes of file sizes in bytes
 SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
             1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
+
+# function that approximates the size of the file given
+def approx_size(size, a_kilobyte_is_1024_bytes = True):
+    # if statement to check the size is not a positive number
+    if size < 0:
+        raise ValueError('Number entered must be positive')
+
+    # sets multiple to 1024 if the above second argument is true
+    multiple = 1024 if a_kilobyte_is_1024_bytes else 1000
+    for suffix in SUFFIXES[multiple]:
+        # gets the value in the array that is a division of whatever the size is
+        size /=multiple
+        if size < multiple:
+            return '{0:.1f} {1}'.format(size, suffix)
+    
+    raise ValueError('That Number is too Large')
+
+if __name__ == '__main__':
+    print(approx_size(1000000000000, False))
+    print(approx_size(1000000000000))
+    print(approx_size(1000, False))
+
